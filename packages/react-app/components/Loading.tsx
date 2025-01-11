6d5137cb3f3db3d9ec092c513b29f1f78b0ffde6
Godwin <godwinaquinas@gmail.com>
2025-01-11 20:39:35 +0100

    fix loading m3terHead size

--- packages/react-app/components/Loading.tsx
+++ packages/react-app/components/Loading.tsx
@@ -6,7 +6,7 @@ export default function Loading() {
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
       <div className="hithere">
-        <M3terHead seed="load" />
+        <M3terHead seed="load" size={120} />
       </div>
     </div>
   )
