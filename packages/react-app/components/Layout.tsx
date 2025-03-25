c5cccc420632a2039565e28101742ffd976d3c99
Godwin <godwinaquinas@gmail.com>
2025-03-25 13:27:49 +0100

    initial commit

--- packages/react-app/components/Layout.tsx
+++ packages/react-app/components/Layout.tsx
@@ -9,7 +9,7 @@ const Layout: FC<Props> = ({ children, loading }) => {
   return (
     <>
       { loading && <Loading />}
-      <div className="bg-[#332807] max-w-sm flex justify-center flex-col min-h-screen">
+      <div className="bg-[#332807] flex justify-center flex-col min-h-screen">
         {/* <Header /> */}
         <div className="mx-auto space-y-8 sm:px-6 lg:px-8">
           {children}
