8646f634e0156d098b000224f27dec757f36adcb
Godwin <godwinaquinas@gmail.com>
2025-04-14 08:40:33 +0100

    test minipay connection

--- packages/react-app/contexts/useWeb3.ts
+++ packages/react-app/contexts/useWeb3.ts
@@ -24,7 +24,7 @@ export const useWeb3 = () => {
 
   const [address, setAddress] = useState<string | null>(null);
 
-  const isMinipay = typeof window !== "undefined" && window.ethereum.isMetaMask;
+  const isMinipay = typeof window !== "undefined" && window.ethereum.isMinipay;
 
   const getUserAddress = async () => {
     if (typeof window !== "undefined" && window.ethereum) {
